01b37617ee6580b979e0869f3074453a0829e927
aryan619 <48639166+aryan619@users.noreply.github.com>
2019-03-17 10:43:08 +0530

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://aryan:aryan@cluster0-cihko.mongodb.net/test?retryWrites=true',
+  pusherAppId: '737646',
+  pusherKey: 'bee1da7df11654205c95',
+  pusherSecret: 'cd8f6bc7c1cbd1922c81',
+  pusherCluster: 'ap2',
+  pusherEncrypted: true
+};
